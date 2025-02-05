ce75028ad10b849f6dfdd8abbab646cde9fc7a0f
Syeda <syeda.annie.asad@gmail.com>
2025-02-05 19:18:23 +0200

    Added headless mode

--- src/main/java/com/example/base/WebDriverGenerator.java
+++ src/main/java/com/example/base/WebDriverGenerator.java
@@ -1,6 +1,7 @@
 package com.example.base;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
@@ -9,6 +10,9 @@ public class WebDriverGenerator {
 
     @BeforeClass
     public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
         driver = new ChromeDriver();
     }
     public void navigateTo(String url) {
